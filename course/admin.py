192cdfcab7659d7b585d16f401518ab0b6897e97
flavien-hugs <flavienhugs@pm.me>
2022-01-03 00:27:22 +0000

    create and edit course models

---
+++ course/admin.py
@@ -0,0 +1,68 @@
+# course.admin.py
+
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+
+from course.models import Course
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Course)
+class CourseAdmin(SummernoteModelAdmin):
+    model = Course
+    list_per_page = 10
+    date_hierarchy = "published"
+    fieldsets = (
+        (
+            'Information sur la formation', {
+                'fields': (
+                    "name", "subtitle",
+                    "price", "date_of_course",
+                    'location_of_course',
+                )
+            }
+        ),
+        (
+            'Description de la formation', {
+                'fields': (
+                    "resume",
+                    "description",
+                    "cover",
+                    ("option", "status"),
+                    "published"
+                )
+            }
+        ),
+    )
+    list_display = [
+        "name",
+        "view",
+        "status",
+        "option",
+        "show_course_url",
+        "created_at",
+    ]
+    list_display_links = [
+        'name',
+    ]
+    list_filter = (
+        "status",
+        "option",
+    )
+    list_editable = (
+        "option",
+        "status",
+    )
+    search_fields = (
+        "name",
+        "status",
+        "option",
+    )
+    
+    @mark_safe
+    @admin.display(description="Voir la formation")
+    def show_course_url(self, instance):
+        url = instance.get_absolute_url()
+        response = format_html(f"""<a target="_blank" href="{url}">{url}</a>""")
+        return response
---
+++ course/models.py
@@ -0,0 +1,141 @@
+# course.models.py
+
+from django.db import models
+from django.urls import reverse
+from django.contrib import admin
+from django.dispatch import receiver
+from django.utils.text import slugify
+from django.utils.crypto import get_random_string
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+from course.managers import CourseManager
+from common.models import BaseTimeStampModel, UUIDSlugMixin
+
+
+NULL_AND_BLANK = {'null': True, 'blank': True}
+
+
+class Course(UUIDSlugMixin, BaseTimeStampModel):
+
+    P = 'Publié'
+    B = 'Brouillon'
+    R = 'Relecture'
+
+    T = 'En Ligne'
+    O = 'En Présentiel'
+
+    STATUS_CHOICES = (
+        (P, 'Publié'),
+        (B, 'Brouillon')
+    )
+
+    OPTION_COURSE_CHOICES = (
+        (T, 'En Ligne'),
+        (O, 'En Présentiel')
+    )
+
+    name = models.CharField(
+        max_length=200,
+        verbose_name="titre",
+        help_text='Saisir le titre de cette formation (200 caractères maximum).'
+    )
+    subtitle = models.CharField(
+        verbose_name='sous-titre',
+        max_length=180,
+        help_text='Saisir le sous-titre de cette formation (200 caractères maximum).',
+        **NULL_AND_BLANK
+    )
+    price = models.PositiveIntegerField(
+        default=2000,
+        verbose_name='côut de la formation',
+        validators=[
+            MinValueValidator(2000),
+            MaxValueValidator(1000000)
+        ],
+        help_text='Indiquer le côut de cette formation.'
+    )
+    date_of_course = models.DateTimeField(
+        auto_now_add=False, auto_now=False,
+        verbose_name='date & heure de la formation',
+        help_text="Indiquer la date & l'heure de la formation"
+    )
+    location_of_course = models.CharField(
+        max_length=200,
+        verbose_name='Lieux de formation',
+        help_text="Définir le lieux de la formation."
+    )
+    resume = models.TextField(
+        max_length=300,
+        verbose_name='résumé',
+        help_text='faire un petit résumé de cette formation.',
+        **NULL_AND_BLANK
+    )
+    description = models.TextField(
+        verbose_name='description',
+        help_text='décrire la formation, les rubriques et les objectifs.',
+        **NULL_AND_BLANK
+    )
+    cover = models.ImageField(
+        upload_to="formation/",
+        verbose_name="ajouter une image",
+        help_text="ajouter une image descriptive de l'article.",
+        **NULL_AND_BLANK
+    )
+    view = models.PositiveIntegerField(
+        default=0,
+        editable=False,
+        verbose_name="nombre de vues"
+    )
+    option = models.CharField(
+        default=O,
+        max_length=13,
+    	verbose_name="Option de la formation",
+    	choices=OPTION_COURSE_CHOICES,
+        help_text="Option de la formation"
+    )
+    status = models.CharField(
+        default=B,
+        max_length=10,
+    	verbose_name="status",
+    	choices=STATUS_CHOICES,
+        help_text="status de la formation."
+    )
+    published = models.DateTimeField(
+        auto_now_add=False, auto_now=False,
+        verbose_name='date et de publication',
+        help_text="Programmé la date et l'heure de la formation."
+    )
+
+    objects = CourseManager()
+
+    class Meta:
+        ordering = ['-published']
+        verbose_name_plural = 'formations'
+        indexes = [models.Index(fields=['uuid'])]
+
+    def __str__(self):
+        return self.name
+
+    def clean(self):
+        if (
+            self.published >= self.date_of_course
+        ):
+            raise ValidationError(
+                {
+                    "published": "La date de publication ne doit pas \
+                    être supérieure ou égale à la date de début de formation."
+                }
+            )
+    
+    def _get_unique_slug(self):
+        slug = slugify(self.name)
+        unique_slug = slug
+        while Course.objects.filter(slug=unique_slug).exists():
+            unique_slug = f"{slug}-{get_random_string(6)}".lower()
+        return unique_slug
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self._get_unique_slug()
+        super().save(*args, **kwargs)
