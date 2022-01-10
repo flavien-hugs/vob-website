f3ed81ec007c293287d79d76a20f8ca9d31e54b1
flavien-hugs <flavienhugs@pm.me>
2022-01-10 14:36:58 +0000

    edit book and course views

---
+++ course/views/book.py
@@ -0,0 +1,40 @@
+# course.views.py
+
+from django.views import generic
+from django.views.generic.detail import SingleObjectMixin
+
+from course.models import Book
+
+
+class BookListView(generic.ListView):
+    paginate_by = 6
+    queryset = Book.objects.published()
+    template_name = "book/book_list.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs['page_title'] = "Livre en vente"
+        return super().get_context_data(**kwargs)
+
+
+book_list_view = BookListView.as_view()
+
+
+class BookDetailView(SingleObjectMixin, generic.ListView):
+    paginate_by = 2
+    template_name = "book/book_detail.html"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(
+            queryset=Book.objects.published()
+        )
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        kwargs['page_title'] = f"{self.object.name}"
+        return super().get_context_data(**kwargs)
+    
+    def get_queryset(self):
+        return Book.objects.published()
+
+
+book_detail_view = BookDetailView.as_view()
---
+++ course/views/course.py
@@ -0,0 +1,40 @@
+# course.views.py
+
+from django.views import generic
+from django.views.generic.detail import SingleObjectMixin
+
+from course.models import Course
+
+
+class CourseListView(generic.ListView):
+    paginate_by = 6
+    queryset = Course.objects.published()
+    template_name = "course/course_list.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs['page_title'] = "Formations"
+        return super().get_context_data(**kwargs)
+
+
+course_list_view = CourseListView.as_view()
+
+
+class CourseDetailView(SingleObjectMixin, generic.ListView):
+    paginate_by = 2
+    template_name = "course/course_detail.html"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(
+            queryset=Course.objects.published()
+        )
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        kwargs['page_title'] = f"{self.object.name}"
+        return super().get_context_data(**kwargs)
+    
+    def get_queryset(self):
+        return Course.objects.published()
+
+
+course_detail_view = CourseDetailView.as_view()
