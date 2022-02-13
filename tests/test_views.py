4670664919ae478f340c5a8b9fa18670d01c211a
flavien-hugs <flavienhugs@pm.me>
2022-02-13 20:13:22 +0000

    edit test for robots

---
+++ tests/test_views.py
@@ -0,0 +1,19 @@
+# tests.test_views.py
+
+from http import HTTPStatus
+
+from django.test import TestCase
+
+
+class RobotsTest(TestCase):
+    def test_get(self):
+        response = self.client.get("/robots.txt")
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response["content-type"], "text/plain")
+        lines = response.content.decode().splitlines()
+        self.assertEqual(lines[0], "User-Agent: *")
+
+    def test_post(self):
+        response = self.client.post("/robots.txt")
+        self.assertEqual(response.status_code, HTTPStatus.METHOD_NOT_ALLOWED)
