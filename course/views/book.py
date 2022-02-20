6406fff8c9e1e586aeb669d3bf67a7ef8de6ed89
flavien-hugs <geekactux@gmail.com>
2022-02-20 23:22:06 +0000

    rename book template

--- course/views/book.py
+++ course/views/book.py
@@ -9,7 +9,7 @@ from course.models import Book
 class BookListView(generic.ListView):
     paginate_by = 6
     queryset = Book.objects.published()
-    template_name = "book/book_list.html"
+    template_name = "book/_list.html"
 
     def get_context_data(self, **kwargs):
         kwargs['page_title'] = "Livre en vente"
@@ -21,7 +21,7 @@ book_list_view = BookListView.as_view()
 
 class BookDetailView(SingleObjectMixin, generic.ListView):
     paginate_by = 2
-    template_name = "book/book_detail.html"
+    template_name = "book/_detail.html"
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object(
@@ -32,7 +32,7 @@ class BookDetailView(SingleObjectMixin, generic.ListView):
     def get_context_data(self, **kwargs):
         kwargs['page_title'] = f"{self.object.name}"
         return super().get_context_data(**kwargs)
-    
+
     def get_queryset(self):
         return Book.objects.published()
 
