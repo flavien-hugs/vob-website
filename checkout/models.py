1c810adf5ed0f494c88e760a6c9d6afa23286e36
flavien-hugs <geekactux@gmail.com>
2022-02-21 04:18:52 +0000

    add method into checkout

--- checkout/models.py
+++ checkout/models.py
@@ -1,10 +1,14 @@
 # checkout.models.py
 
 import uuid
+import string
+import random
 import datetime
+from decimal import Decimal
 
 from django.db import models
-
+from django.urls import reverse
+from django.contrib import admin
 from blog.models import Post
 from course.models import Book
 from common.models import BaseTimeStampModel, UserBaseInfo
@@ -12,18 +16,9 @@ from common.models import BaseTimeStampModel, UserBaseInfo
 NULL_AND_BLANK = {'null': True, 'blank': True}
 
 
-def increment_order_number():
-    year = str(datetime.date.today().year)
-    month = str(datetime.date.today().month).zfill(2)
-    last_order = Checkout.objects.order_by('uuid').last()
-    if not last_order:
-        return '' + year + month + '0000'
-    
-    order_number = last_order.order_number
-    order_int = int(order_number[9:13])
-    new_order_int = order_int + year + month + str(new_order_int).zfill(4)
-    
-    return new_order_id
+def random_order_code():
+    order_code = "".join(random.choices(string.digits, k=8))
+    return order_code
 
 
 class Checkout(UserBaseInfo, BaseTimeStampModel):
@@ -35,41 +30,40 @@ class Checkout(UserBaseInfo, BaseTimeStampModel):
         verbose_name='uuid'
     )
     id_checkout = models.CharField(
-        max_length=500,
-        verbose_name='ID Paiement',
+        max_length=8,
+        verbose_name='ID Commande',
         editable=False, unique=True,
-        default=increment_order_number
-    )
-    post = models.ForeignKey(
-        to=Post,
-        on_delete=models.SET_NULL,
-        related_name='checkout_post',
-        verbose_name='article',
-        **NULL_AND_BLANK
+        default=random_order_code
     )
     book = models.ForeignKey(
         to=Book,
-        on_delete=models.SET_NULL,
-        related_name='checkout_book',
+        on_delete=models.CASCADE,
+        related_name='books',
         verbose_name='livre',
-        **NULL_AND_BLANK
     )
-    quantity = models.PositiveIntegerField(
-        verbose_name='quantité',
-        default=1
+    ip_address = models.GenericIPAddressField(
+        max_length=180,
+        protocol='both',
+        unpack_ipv4=False,
+        verbose_name='adresse ip',
+        **NULL_AND_BLANK
     )
 
     class Meta:
         ordering = ['-created_at']
         get_latest_by = ['-created_at']
-        verbose_name_plural = "commande"
+        verbose_name_plural = "commande de livre"
         indexes = [models.Index(fields=['uuid'])]
 
     def __str__(self):
         return f"{self.first_name} - {self.phone}- {self.id_checkout}"
 
-    def get_post_cost(self):
-        return self.post.price * self.quantity
-    
+    @admin.display(description="prix")
     def get_book_cost(self):
-        return self.book.price * self.quantity
+        return f"{self.book.price} frcfa".upper()
+
+    def get_absolute_url(self):
+        return reverse(
+            "checkout:checkout_path",
+            kwargs={"slug": self.book.slug}
+        )
