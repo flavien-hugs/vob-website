3e5c489be68c8cc72e142f106f70cc75a9af97ae
flavien-hugs <geekactux@gmail.com>
2022-02-23 18:07:21 +0000

    improve global urls

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,7 @@ from django.conf.urls.static import static
 from django.contrib.auth.models import Group
 
 # admin.site.unregister(Site)
-# admin.site.unregister(Group)
+admin.site.unregister(Group)
 
 admin.site.site_header = admin.site.site_title = "Valere Obei"
 admin.site.index_title = "Bienvenu sur votre tableau d'administration".capitalize()
