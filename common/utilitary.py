8141fc0f543082c30472f89b7c15edbf55303130
flavien-hugs <geekactux@gmail.com>
2022-03-01 23:59:49 +0000

    edit cache

--- common/utilitary.py
+++ common/utilitary.py
@@ -1,9 +1,13 @@
 # common.utilitary.py
 
 import os
+import logging
+from hashlib import sha256
 
 from django.utils.text import slugify
 from django.utils.crypto import get_random_string
+
+logger = logging.getLogger(__name__)
 
 
 def upload_image_to(instance, filename):
@@ -34,3 +38,35 @@ def get_client_ip(request):
     else:
         ip = request.META.get('REMOTE_ADDR', None)
     return ip
+
+
+def cache_decorator(expiration=3 * 60):
+    def wrapper(func):
+        def news(*args, **kwargs):
+            try:
+                view = args[0]
+                key = view.get_cache_key()
+            except BaseException:
+                key = None
+            if not key:
+                unique_str = repr((func, args, kwargs))
+
+                m = sha256(unique_str.encode('utf-8'))
+                key = m.hexdigest()
+            value = cache.get(key)
+            if value is not None:
+                if str(value) == '__default_cache_value__':
+                    return None
+                else:
+                    return value
+            else:
+                logger.info(f"cache_decorator set cache:{func.__name__} key:{key}")
+                value = func(*args, **kwargs)
+                if value is None:
+                    cache.set(key, '__default_cache_value__', expiration)
+                else:
+                    cache.set(key, value, expiration)
+                return value
+        return news
+
+    return wrapper
